aa44af972dc6e4e45f856c19fb6087105394df26
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-06-14 12:08:33 +0200

    removed notifications fragment

--- app/src/main/java/com/peternaggschga/sleeptalk/MainActivity.kt
+++ app/src/main/java/com/peternaggschga/sleeptalk/MainActivity.kt
@@ -25,8 +25,7 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_home, R.id.navigation_dashboard))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
