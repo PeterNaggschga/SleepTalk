57ad88f9b6e098951833d3a387285b157204d622
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-06-26 00:19:13 +0200

    added AudioAccumulator#frameList
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/sleeptalk/domain/monitoring/AudioAccumulator.kt
+++ app/src/main/java/com/peternaggschga/sleeptalk/domain/monitoring/AudioAccumulator.kt
@@ -7,8 +7,6 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.launch
-import kotlin.math.abs
-import kotlin.math.max
 
 class AudioAccumulator(
     private val inputChannel: Channel<FloatArray>,
@@ -18,28 +16,39 @@ class AudioAccumulator(
         private const val INPUT_CHANNEL_BUFFER_SIZE = 60
 
         fun getInputChannel() = Channel<FloatArray>(INPUT_CHANNEL_BUFFER_SIZE)
+
+        private const val PURGE_THRESHOLD = 60 * 60
     }
 
     private lateinit var distribution: Distribution
+    private lateinit var frameList: MutableList<Recording>
 
     suspend fun accumulate() = calculationScope.launch {
         distribution = Distribution()
+        frameList = mutableListOf()
         for (element in inputChannel) {
             ensureActive()
             processElement(element)
         }
+        purgeFrameList()
     }
 
     private fun processElement(element: FloatArray) {
-        val maxValue = max(
-            abs(element.max()),
-            abs(element.min())
-        )
-        distribution.addValue(maxValue.toDouble())
+        val recording = Recording(element, distribution.numberOfValues + 1)
+        distribution.addValue(recording.maxValue.toDouble())
+        frameList.add(recording)
+
+        if (distribution.numberOfValues > PURGE_THRESHOLD) {
+            purgeFrameList()
+        }
 
         Log.d(
             "AudioAccumulator",
-            "Distribution: maxValue = $maxValue, n = ${distribution.numberOfValues}, mean = ${distribution.mean}, stddev = ${distribution.stddev}"
+            "Distribution: new = ${recording.maxValue}, max = ${distribution.max}, n = ${distribution.numberOfValues}, mean = ${distribution.mean}, stddev = ${distribution.stddev}"
         )
+    }
+
+    private fun purgeFrameList() {
+        TODO()
     }
 }
