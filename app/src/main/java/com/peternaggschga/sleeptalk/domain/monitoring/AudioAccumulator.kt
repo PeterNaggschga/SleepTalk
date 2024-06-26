0518dfd86b1405e0cdde462f9b946c8d7c66e77e
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-06-26 23:42:27 +0200

    removed Distribution.kt
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/sleeptalk/domain/monitoring/AudioAccumulator.kt
+++ app/src/main/java/com/peternaggschga/sleeptalk/domain/monitoring/AudioAccumulator.kt
@@ -1,6 +1,5 @@
 package com.peternaggschga.sleeptalk.domain.monitoring
 
-import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
@@ -16,39 +15,21 @@ class AudioAccumulator(
         private const val INPUT_CHANNEL_BUFFER_SIZE = 60
 
         fun getInputChannel() = Channel<FloatArray>(INPUT_CHANNEL_BUFFER_SIZE)
-
-        private const val PURGE_THRESHOLD = 60 * 60
     }
 
-    private lateinit var distribution: Distribution
-    private lateinit var frameList: MutableList<Recording>
+    private lateinit var signalDetection: SignalDetection
 
     suspend fun accumulate() = calculationScope.launch {
-        distribution = Distribution()
-        frameList = mutableListOf()
+        signalDetection = SignalDetection()
         for (element in inputChannel) {
             ensureActive()
-            processElement(element)
+
+            val recording = Recording(element, signalDetection.n)
+            signalDetection.addValue(recording.maxValue.toDouble())
+
+            if (signalDetection.currentSignal) {
+                TODO()
+            }
         }
-        purgeFrameList()
-    }
-
-    private fun processElement(element: FloatArray) {
-        val recording = Recording(element, distribution.numberOfValues + 1)
-        distribution.addValue(recording.maxValue.toDouble())
-        frameList.add(recording)
-
-        if (distribution.numberOfValues > PURGE_THRESHOLD) {
-            purgeFrameList()
-        }
-
-        Log.d(
-            "AudioAccumulator",
-            "Distribution: new = ${recording.maxValue}, max = ${distribution.max}, n = ${distribution.numberOfValues}, mean = ${distribution.mean}, stddev = ${distribution.stddev}"
-        )
-    }
-
-    private fun purgeFrameList() {
-        TODO()
     }
 }
--- app/src/main/java/com/peternaggschga/sleeptalk/domain/monitoring/SignalDetection.kt
+++ app/src/main/java/com/peternaggschga/sleeptalk/domain/monitoring/SignalDetection.kt
@@ -1,6 +1,5 @@
 package com.peternaggschga.sleeptalk.domain.monitoring
 
-import androidx.core.util.Consumer
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics
 import kotlin.math.abs
 
@@ -8,13 +7,14 @@ import kotlin.math.abs
  * https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data
  */
 class SignalDetection(
-    private val onSignalCallback: Consumer<Boolean>,
     private val lag: Long = 60L * 10 / MonitoringServiceHandler.SECONDS_PER_FRAME,
     private val threshold: Double = 3.0,
     private val influence: Double = 0.5
 ) {
     private val stats = SummaryStatistics()
-    private var currentSignal = false
+    var currentSignal = false
+        private set
+    val n get() = stats.n
 
     fun addValue(value: Double) {
         if (stats.n < lag) {
@@ -32,7 +32,6 @@ class SignalDetection(
 
         if (signal != currentSignal) {
             currentSignal = signal
-            onSignalCallback.accept(currentSignal)
         }
     }
 }
