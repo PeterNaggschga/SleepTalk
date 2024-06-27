842b77f27fd424bad5892475409a73dd2f43ea72
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-06-27 10:50:23 +0200

    fixed influence of lag parameter in SignalDetection
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

--- app/src/main/java/com/peternaggschga/sleeptalk/domain/monitoring/SignalDetection.kt
+++ app/src/main/java/com/peternaggschga/sleeptalk/domain/monitoring/SignalDetection.kt
@@ -7,23 +7,23 @@ import kotlin.math.abs
  * https://stackoverflow.com/questions/22583391/peak-signal-detection-in-realtime-timeseries-data
  */
 class SignalDetection(
-    private val lag: Long = 60L * 10 / MonitoringServiceHandler.SECONDS_PER_FRAME,
+    private val lag: Int = 60 * 10 / MonitoringServiceHandler.SECONDS_PER_FRAME,
     private val threshold: Double = 3.0,
     private val influence: Double = 0.5
 ) {
     private val stats = SummaryStatistics()
     var currentSignal = false
         private set
-    val n get() = stats.n
+    private val lagValues: ArrayDeque<Double> = ArrayDeque(lag + 1)
 
     fun addValue(value: Double) {
-        if (stats.n < lag) {
-            stats.addValue(value)
+        if (lagValues.size < lag) {
+            addLagValue(value)
             return
         }
 
         val signal = abs(value - stats.mean) > threshold * stats.standardDeviation
-        stats.addValue(
+        addLagValue(
             if (signal)
                 value * influence
             else
@@ -32,6 +32,17 @@ class SignalDetection(
 
         if (signal != currentSignal) {
             currentSignal = signal
+        }
+    }
+
+    private fun addLagValue(value: Double) {
+        lagValues.addLast(value)
+        if (lagValues.size > lag) {
+            lagValues.removeFirst()
+            stats.clear()
+            lagValues.forEach { lagValue -> stats.addValue(lagValue) }
+        } else {
+            stats.addValue(value)
         }
     }
 }
