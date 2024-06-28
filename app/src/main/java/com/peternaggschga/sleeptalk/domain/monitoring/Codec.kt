cc5eb19db3508cab9d7beb2f0cabcf553bfc9150
PeterNaggschga <rep.akhcsztan@gmail.com>
2024-06-28 11:54:09 +0200

    added Codec.kt
    Signed-off-by: PeterNaggschga <rep.akhcsztan@gmail.com>

---
+++ app/src/main/java/com/peternaggschga/sleeptalk/domain/monitoring/Codec.kt
@@ -0,0 +1,32 @@
+package com.peternaggschga.sleeptalk.domain.monitoring
+
+import java.io.BufferedOutputStream
+import java.io.DataOutputStream
+import java.io.File
+import java.io.FileOutputStream
+
+abstract class Codec(private val rootDirectory: File) {
+    abstract val fileExtension: String
+
+    fun savePcmToFile(pcm: FloatArray, fileName: String) {
+        if (!rootDirectory.exists()) {
+            rootDirectory.mkdirs()
+        }
+
+        val file = File(rootDirectory, fileName + fileExtension)
+        file.createNewFile()
+
+        val outStream = DataOutputStream(
+            BufferedOutputStream(
+                FileOutputStream(
+                    file
+                )
+            )
+        )
+        writeAudioData(pcm, outStream)
+        outStream.flush()
+        outStream.close()
+    }
+
+    abstract fun writeAudioData(pcm: FloatArray, outputStream: DataOutputStream)
+}
